d7e1b04942e681fd38480fb572b783d98eb74d5f
jim berry <jim@bigfattail.com>
2020-01-07 15:58:01 -0500

    Fixed remote bike creation

--- FrontendBike.cs
+++ FrontendBike.cs
@@ -9,8 +9,20 @@ namespace BeamCli
     {
         public static FrontendBike Create(IBike ib)
         {
-            System.Type bType = FrontendBike.bikeClassTypes[ib.ctrlType];
-            return (FrontendBike)Activator.CreateInstance(bType);
+            FrontendBike feb = null;
+            switch (ib.ctrlType)
+            {
+            case BikeFactory.LocalPlayerCtrl:
+                feb = new PlayerBike();
+                break;                
+            case BikeFactory.AiCtrl:
+                feb = new AiBike();
+                break;
+            case BikeFactory.RemoteCtrl:
+                feb = new RemoteBike();
+                break;
+            }
+            return feb;
         }
     }
 
@@ -21,8 +33,6 @@ namespace BeamCli
             typeof(AiBike),  // AiCtrl - AI - controlled local bike
             typeof(PlayerBike) // LocalPlayerCtrl - a human on this machine
         };
-
-
 
         public IBike bb = null;
         protected IBeamBackend be = null; 
@@ -60,4 +70,13 @@ namespace BeamCli
             control = new PlayerControl();
         }        
     }    
+
+    public class RemoteBike : FrontendBike
+    {
+        protected override void CreateControl()
+        {
+            Debug.Log(string.Format("PlayerBike.RemoteBike()"));        
+            control = new RemoteControl();
+        }        
+    }     
 }
